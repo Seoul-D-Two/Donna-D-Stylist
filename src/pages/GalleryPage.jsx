173c9352cf83973ba4fc2037f3172a94da4809c0
Sulnklm <hyeinsuin@icloud.com>
2025-01-19 21:33:20 -0800

    gallery page update

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import Slider from "react-slick"; // Slick Slider 임포트
+import "slick-carousel/slick/slick.css"; // Slick 스타일
+import "slick-carousel/slick/slick-theme.css"; // Slick 테마 스타일
+import Section5 from "../components/home/Section5";
 
 export default function Gallery() {
-  // 이미지 경로 배열 생성
   const [imageUrls, setImageUrls] = useState([]);
 
   useEffect(() => {
@@ -17,33 +20,115 @@ export default function Gallery() {
     window.open("https://www.instagram.com/donnadstylistsalon/", "_blank"); // 클릭 시 Instagram 열기
   };
 
+
+  const settings1 = {
+    infinite: true, 
+    speed: 10000, 
+    slidesToShow: 4.5, 
+    slidesToScroll: 1, 
+    autoplay: true, 
+    autoplaySpeed: 0,
+    arrows: false,
+    rtl: true,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3.5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.5,
+        },
+      },
+    ],
+  };
+
+  const settings2 = {
+    infinite: true, 
+    speed: 10000, 
+    slidesToShow: 4.5, 
+    slidesToScroll: 1, 
+    autoplay: true, 
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    arrows: false,
+    rtl: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3.5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.5,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="bg-customGrayLight py-[8rem] min-h-screen">
-      <div className="container mx-auto grid gap-10 lg:gap-20">
+    <div className="bg-customGrayLight pt-[8rem] min-h-screen">
+      <div className="container mx-auto pb-20">
         <h1 className="border-b border-b-customGrayDark w-fit mx-auto pb-3 text-3xl text-center">
           GALLERY
         </h1>
-      <div className="grid grid-cols-4 gap-4">
-        {imageUrls.map((url, index) => (
-          <div
-            key={index}
-            className="w-48 h-48 bg-gray-300 flex items-center justify-center border border-gray-300"
-            onClick={handleImageClick}
-          >
-            <img
-              src={url}
-              alt={`Gallery Image ${index + 1}`}
-              className="object-cover w-full h-full"
-              onError={(e) => {
-                // 이미지 로드 실패 시 대체 이미지 표시
-                e.target.src =
-                  "https://via.placeholder.com/150?text=Image+Not+Found";
-              }}
-            />
-          </div>
-        ))}
+
+        {/* Slider 2 */}
+        <Slider {...settings2}>
+        {imageUrls.slice(1, 9).map((url, index) => (
+            <div
+              key={index}
+              className="mt-10 w-full p-2 flex justify-center items-center"
+              onClick={handleImageClick}
+            >
+              <div className="w-full h-full bg-gray-300 flex justify-center items-center">
+                <img
+                  src={url}
+                  alt={`Gallery Image ${index + 1}`}
+                  className="object-cover w-full h-full aspect-square"
+                  onError={(e) => {
+                    // 이미지 로드 실패 시 대체 이미지 표시
+                    e.target.src =
+                      "https://via.placeholder.com/150?text=Image+Not+Found";
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </Slider>
+
+        {/* Slider 1 */}
+        <Slider {...settings1}>
+        {imageUrls.slice(10, 20).map((url, index) => (
+            <div
+              key={index}
+              className="w-full p-2 flex justify-center items-center"
+              onClick={handleImageClick}
+            >
+              <div className="w-full h-full bg-gray-300 flex justify-center items-center">
+                <img
+                  src={url}
+                  alt={`Gallery Image ${index + 1}`}
+                  className="object-cover w-full h-full aspect-square"
+                  onError={(e) => {
+                    // 이미지 로드 실패 시 대체 이미지 표시
+                    e.target.src =
+                      "https://via.placeholder.com/150?text=Image+Not+Found";
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    </div>
+      <Section5 />
     </div>
   );
 }
